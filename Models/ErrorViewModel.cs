d01b1b75a3818175fcb7496fef6559dac0d1d549
[Brody Van Eperen] <bvaneperen@hbs.net>
2024-07-15 13:28:49 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Counselors_Connect.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
