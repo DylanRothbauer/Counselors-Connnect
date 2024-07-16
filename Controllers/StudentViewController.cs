809a243cf05ed9f922b4254e4c92c903a6d7fc75
Landen Hawley <lhawley@hbs.net>
2024-07-16 11:11:45 -0500

    add Navigation to site

---
+++ Controllers/StudentViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Counselors_Connect.Controllers
+{
+    public class StudentViewController : Controller
+    {
+        public IActionResult StudentView()
+        {
+            return View();
+        }
+    }
+}
