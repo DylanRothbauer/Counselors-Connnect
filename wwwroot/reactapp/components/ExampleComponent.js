5083852762e5ce98829c4aebfcc3c9e35c0dbc3b
Dylan Rothbauer <djrothbauer@gmail.com>
2024-07-17 10:25:19 -0500

    Created Demo Page with example component on it for example

--- wwwroot/reactapp/components/ExampleComponent.js
+++ wwwroot/reactapp/components/ExampleComponent.js
@@ -1,1 +1,21 @@
-﻿
+﻿import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Example = () => {
+
+    return (
+        <div>
+            <h3>Example Component</h3>
+        </div>
+    );
+};
+
+export default Example;
+
+//React DOM rendering
+const visits = ReactDOM.createRoot(document.getElementById("example-component"));
+visits.render(
+    <React.StrictMode>
+        <Example />
+    </React.StrictMode>
+);
