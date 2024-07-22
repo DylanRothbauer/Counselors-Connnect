a6e696a95480679267add718cd06a69a4965400a
Landen Hawley <lhawley@hbs.net>
2024-07-22 11:53:09 -0500

    delete multiple models

--- Models/MultipleModels.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Counselors_Connect.Models
-{
-    public class MultipleModels
-    {
-        public IEnumerable<Visit> Visits { get; set; } = new List<Visit>();
-        public IEnumerable<Student> Students { get; set; } = new List<Student>();
-    }
-}
